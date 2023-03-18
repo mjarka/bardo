d235834cae027c203916d1427408146c321a3e1b
Marcin <marcinjarka+heroku@gmail.com>
2023-03-18 11:42:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Model } from "./Model";
+import {
+  Environment,
+  Lightformer,
+  OrbitControls,
+  PivotControls,
+} from "@react-three/drei";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        height: "100vh",
+        backgroundColor: "#e4e4e3",
+        textAlign: "left",
+      }}
+    >
+      <div style={{ flex: 6 }}>
+        <Canvas camera={{ position: [0, 0, 5], fov: 35 }}>
+          <directionalLight castShadow intensity={0.6} position={[0, 0, 10]} />
+
+          <Model />
+          {/* <OrbitControls /> */}
+          <Environment preset="park" />
+        </Canvas>
+      </div>
+      <div
+        style={{
+          flex: 5,
+          padding: "1rem",
+          display: "flex",
+          alignContent: "flex-start",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Text content goes here</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
     </div>
   );
 }
---
+++ src/Model.js
@@ -0,0 +1,41 @@
+import React, { useRef, useState, useEffect } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+export function Model(props) {
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF("/bardoFace.glb");
+  const { actions } = useAnimations(animations, group);
+  const [animationIndex, setAnimationIndex] = useState(0);
+  const currentAnimation = useRef(null);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (currentAnimation.current) {
+        currentAnimation.current.fadeOut(0.5);
+      }
+      const randomIndex = Math.floor(Math.random() * animations.length);
+      const animationAction = actions[animations[randomIndex].name];
+      setAnimationIndex(randomIndex);
+      currentAnimation.current = animationAction.reset().fadeIn(2.5).play();
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useFrame(({ clock }) => {
+    group.current.rotation.y = Math.sin(clock.elapsedTime * 0.6) * 0.1;
+    group.current.rotation.x = Math.sin(clock.elapsedTime * 0.6) * 0.1;
+  });
+
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene">
+        <group name="Armature">
+          <primitive object={nodes.Bone} />
+        </group>
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/bardoFace.glb");
